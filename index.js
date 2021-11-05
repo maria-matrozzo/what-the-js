3a9e308cbd5326a99dbe0a1f7b337b764ab96157
Maria <mmatrozzo@gmail.com>
2021-11-05 12:39:35 -0400

    First commit

---
+++ index.js
@@ -0,0 +1,22 @@
+// console.log("yo")
+
+//Interacting with the DOM
+
+//changing our h1
+let h1Tag = document.querySelector('h1')
+console.log("This is h1Tag ->", h1Tag)
+
+//changing text content
+h1Tag.textContent = "Ellie - The Cutest Dog Ever"
+
+
+//logic section
+let variableOne = 1;
+console.log(variableOne)
+
+variableOne = "one";
+console.log(variableOne)
+
+function changeAVariable(){
+
+}
